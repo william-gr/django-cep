2d339553a436cb46fcc56bcfbd256d1ce193796c
staticdev <thiagocavila@gmail.com>
2013-03-10 10:14:11 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,10 @@ from cep import __version__
 setup(
     name='django-cep',
     version=__version__,
-    author='Thiago Carvalho D Avila',
-    author_email='thiagocavila@gmail.com',
     packages=['cep'],
     scripts=[],
     url='https://github.com/staticdev/django-cep',
-    license='MIT',
+    license='LICENSE.txt',
     description='Address fields autofill app for forms using brazilian CEP field.',
     long_description=open('README.markdown').read() if exists("README.markdown") else "",
     install_requires=[
