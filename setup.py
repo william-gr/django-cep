d2cd138029825f98418b4157b2a5f0d0ec2d6f96
staticdev <thiagocavila@gmail.com>
2013-03-25 22:42:44 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ except ImportError:
 
 setup(
     name='django-cep',
-    version='1.0.1',
+    version='1.0.3',
     maintainer='Thiago Carvalho D Avila',
     maintainer_email='thiagocavila@gmail.com',
     packages=['cep'],
     scripts=[],
     url='https://github.com/staticdev/django-cep',
-    license='LICENSE.txt',
+    license='LICENSE',
     description='Address fields autofill app for forms using brazilian CEP field.',
     long_description=open('README.markdown').read() if exists("README.markdown") else "",
     install_requires=[
