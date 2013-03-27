0d6cab17a6641efbe482d9cb9ba864a03d686b79
staticdev <thiagocavila@gmail.com>
2013-03-27 07:20:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import cep
+
 from os.path import exists
 try:
     from setuptools import setup, find_packages
@@ -8,15 +10,28 @@ except ImportError:
 
 setup(
     name='django-cep',
-    version='1.0.4',
+    version=cep.__version__,
     maintainer='Thiago Carvalho D Avila',
     maintainer_email='thiagocavila@gmail.com',
     packages=find_packages(),
+    include_package_data=True,
     scripts=[],
     url='https://github.com/staticdev/django-cep',
     license='LICENSE',
     description='Address fields autofill app for forms using brazilian CEP field.',
     long_description=open('README.markdown').read() if exists("README.markdown") else "",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
     install_requires=[
         "Django >= 1.2.0"
     ],
