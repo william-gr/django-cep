803c34b2874217a7dc44a049667e96724d704175
Fernando Souza <fernandosouza.eti@gmail.com>
2013-05-27 20:38:07 -0300

    Update views.py
    Correção de encoding, e da linha onde deve ser modificado para utilizar outro website

--- cep/views.py
+++ cep/views.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse
 import urllib2
 import re
 
 
 # Podem ser usados outros webservices como o http://ceplivre.pc2consultoria.com/index.php?module=cep&cep=%s&formato=xml
-# Entretanto deve ser observado o encoding para que ele seja alterado na linha 10
+# Entretanto deve ser observado o encoding para que ele seja alterado na linha 14
 # Outra referencia util http://allissonazevedo.com/2012/03/22/buscando-cep-diretamente-pelo-site-dos-correios-em-python/
 def addressGet(request, zipcode):
     # Trata o zipcode removendo caracteres diferentes de numeros.
